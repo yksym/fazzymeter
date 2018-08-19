e879c1a46679117701edd839aa67f5ae7663fa05
yksym <yksym@none>
2018-08-20 06:26:17 +0900

    toriaezu

--- Src/loop.c
+++ Src/loop.c
@@ -3,6 +3,7 @@
 #include "stm32f4xx_hal.h"
 
 extern UART_HandleTypeDef huart2;
+extern I2C_HandleTypeDef hi2c1;
 
 int _write(int file, char* ptr, int len)
 {
@@ -13,5 +14,11 @@ int _write(int file, char* ptr, int len)
 
 void loop()
 {
-    puts("memo");
+    uint8_t buf[24];
+    while (1) {
+        HAL_StatusTypeDef ret = HAL_I2C_Master_Receive(&hi2c1,(uint16_t)(0b1101000 << 1), buf, sizeof(buf), 1000);
+        if (ret == HAL_OK) {
+            puts("success");
+        }
+    }
 }
